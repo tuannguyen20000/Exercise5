51dbdc122a9cdcfc5eae1ad13c41d84451830677
tuan nguyen <tuannguyen21122000@gmail.com>
2019-10-08 17:38:16 +0700

    Exercise5a

--- Exercise5/Exercise5a/Program.cs
+++ Exercise5/Exercise5a/Program.cs
@@ -10,6 +10,45 @@ namespace Exercise5a
     {
         static void Main(string[] args)
         {
+
+        }
+        public static void PrintABCD()
+        {
+            for (int i = 97; i < 123; i++)
+            {
+                Console.WriteLine((char)i);
+            }
+            for (int i = 122; i > 96; i--)
+            {
+                Console.WriteLine((char)i);
+            }
+            for (int i = 65; i < 91; i++)
+            {
+                Console.WriteLine((char)i);
+            }
+
+            for (int i = 128; i < 255; i++)
+            {
+                Console.WriteLine((char)i);
+            }
+
+        }
+
+        public static bool IsUper(char c)
+        {
+            return false;
+        }
+        public static bool IsLower(char c)
+        {
+            return false;
+        }
+        public static char ToUper(char c)
+        {
+            return c;
+        }
+        public static char ToLower(char c)
+        {
+            return c;
         }
     }
 }
